d7aaf23fa6cb26e3fa6039000c4a170e95ed2dc7
Greg Leppert <greg@leppert.me>
2021-02-23 10:05:31 -0500

    Store user bootstrap data in its own Vuex store

--- web/frontend/src/store/index.ts
+++ web/frontend/src/store/index.ts
@@ -1,10 +1,12 @@
 import { createStore } from 'vuex'
 import globals from './modules/globals'
+import user from './modules/user'
 import captures from './modules/captures'
 
 export default createStore({
-    modules: {
-        globals,
-        captures
-    }
+  modules: {
+    globals,
+    user,
+    captures
+  }
 })
---
+++ web/frontend/src/store/modules/user.ts
@@ -0,0 +1,20 @@
+const state = {
+  ...store_bootstrap.user,
+}
+
+const getters = {
+}
+
+const actions = {
+}
+
+const mutations = {
+}
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations
+}
