df36ff53cd17cd920b81b9143f1423d5a1d08c1d
Greg Leppert <greg@leppert.me>
2021-02-23 10:05:55 -0500

    Build out main nav dropdown

--- web/frontend/src/components/TheMainNav.vue
+++ web/frontend/src/components/TheMainNav.vue
@@ -1,18 +1,42 @@
 <template>
-  <nav aria-label="Main">
-    <ul>
-      <li><a href="/user/account/">My Account</a></li>
-      <li><a href="/docs/">User Guide</a></li>
-      <li><a href="/user/logout/">Log Out</a></li>
-      
-      <li id="admin-only">Admin Only</li>
-      <li><a href="/admin/">Django Admin</a></li>
-      <li><a href="/manage/celery/">Celery Queue Status</a></li>      
-    </ul>
-  </nav>
+<div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+    {{ first_name }}
+  </button>
+  <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton1">
+    <template v-if="is_authenticated">
+      <li><a class="dropdown-item" href="/user/account/">My Account</a></li>
+      <li><a class="dropdown-item" href="/docs/">User Guide</a></li>
+      <li><a class="dropdown-item" href="/user/logout/">Log Out</a></li>
+    </template>
+
+    <template v-else>
+      <li><a class="dropdown-item" href="/sign-up/">Sign up</a></li>
+      <li><a class="dropdown-item" href="/docs/">User Guide</a></li>
+      <li><a class="dropdown-item" href="/user/login">Log in</a></li>
+    </template>
+    
+    <template v-if="is_staff">
+      <li><hr class="dropdown-divider"></li>
+      <li><h6 class="dropdown-header">Admin Only</h6></li>
+      <li><a class="dropdown-item" href="/admin/">Django Admin</a></li>
+      <li><a class="dropdown-item" href="/manage/celery/">Celery Queue Status</a></li>
+    </template>
+  </ul>
+</div>
 </template>
 
 <script lang="ts">
+import { createNamespacedHelpers } from 'vuex'
+const { mapState } = createNamespacedHelpers('user')
+
+export default {
+  computed: mapState([
+    'first_name',
+    'is_authenticated',
+    'is_staff'
+  ])
+}
 </script>
 
 <style scoped>
--- web/frontend/src/components/TheNavbar.vue
+++ web/frontend/src/components/TheNavbar.vue
@@ -1,14 +1,7 @@
 <template>
 <nav class="navbar mb-3 py-3">
   <a href="/" class="navbar-brand">{{ app_name }}</a>
-  <div class="dropdown">
-    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false"></button>
-    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-      <li>
-        Things
-      </li>
-    </ul>
-  </div>
+  <TheMainNav/>
 </nav>
 </template>
 
