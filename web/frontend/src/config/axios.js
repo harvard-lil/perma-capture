b42cc66fe6c462522beef6a53875fc2278297e81
Greg Leppert <greg@leppert.me>
2021-03-01 09:20:47 -0500

    Serialize arrays for the API using comma format

--- web/frontend/src/config/axios.js
+++ web/frontend/src/config/axios.js
@@ -1,12 +1,18 @@
+import qs from "qs";
 import AxiosConfig from "axios";
-import { getCookie } from '../lib/helpers';
+import { getCookie } from "../lib/helpers";
 
 let headers = {"Content-Type": "application/json",
                "Accept": "application/json"};
 
-const csrf_token = getCookie('csrftoken');
+const csrf_token = getCookie("csrftoken");
 if(csrf_token) headers["X-CSRFToken"] = csrf_token;
 
-const Axios = AxiosConfig.create({headers: headers});
+const Axios = AxiosConfig.create({
+    headers: headers,
+    paramsSerializer(params) {
+        return qs.stringify(params, {arrayFormat: "comma"});
+    }
+});
 
 export default Axios;
