654fdd186b0a36c2391157fc86e1e6e19e6caced
Greg Leppert <greg@leppert.me>
2021-03-09 09:56:10 -0500

    Revise template for main header

--- web/frontend/src/components/TheDashboard.vue
+++ web/frontend/src/components/TheDashboard.vue
@@ -1,14 +1,15 @@
 <template>
 <TheNavbar/>
 <main>
-  <div class="mb-5">
-    <h1 class="text-center display-5">Create a new archive</h1>
-    <p class="text-center">Enter any URL to capture it in a downloadable archive.</p>
-  </div>
-  <div class="mb-5">
+  <TheMainHeader
+    head="Create a new archive"
+    subhead="Enter any URL to capture it in a downloadable archive."/>
+
+  <div class="mt-5">
     <CaptureForm/>
   </div>
-  <div class="mb-5">
+
+  <div class="mt-5">
     <CaptureList/>
   </div>
 </main>
@@ -16,12 +17,14 @@
 
 <script lang="ts">
 import TheNavbar from './TheNavbar.vue'
+import TheMainHeader from './TheMainHeader.vue'
 import CaptureForm from './CaptureForm.vue'
 import CaptureList from './CaptureList.vue'
 
 export default {
   components: {
     TheNavbar,
+    TheMainHeader,
     CaptureForm,
     CaptureList
   },
---
+++ web/frontend/src/components/TheMainHeader.vue
@@ -0,0 +1,16 @@
+<template>
+<h1 class="text-center display-5">{{ head }}</h1>
+<p class="text-center" v-if="subhead">{{ subhead }}</p>
+</template>
+
+<script lang="ts">
+export default {
+  props: [
+    'head',
+    'subhead'
+  ]
+}
+</script>
+
+<style scoped>
+</style>
