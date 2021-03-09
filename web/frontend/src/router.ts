c231a27a6d33281b7b62b95e7826db561639a6c2
Greg Leppert <greg@leppert.me>
2021-03-09 09:56:28 -0500

    Begin work on account settings page

--- web/frontend/src/router.ts
+++ web/frontend/src/router.ts
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import TheDashboard from './components/TheDashboard.vue'
 import TheDocs from './components/TheDocs.vue'
+import TheAccountSettings from './components/TheDocs.vue'
 
 const routes = [
   { path: '/', name: 'dashboard', component: TheDashboard },
-  { path: '/docs/', name: 'docs', component: TheDocs }
+  { path: '/docs/', name: 'docs', component: TheDocs },
+  { path: '/user/account/', name: 'account', component: TheAccountSettings }
 ]
 
 export default createRouter({
