57a88bef25f0f46e9e7baf5f0a36e7f6b8b6f3f7
Greg Leppert <greg@leppert.me>
2021-03-13 15:45:22 -0500

    Begin building out the user form

--- web/frontend/src/components/TheAccountSettingsPage.vue
+++ web/frontend/src/components/TheAccountSettingsPage.vue
@@ -4,9 +4,7 @@
   <div class="row mt-5 gx-5">
     <div class="col">
       <h2>Profile</h2>
-      <form method="POST" class="u-full-width">
-        <button type="submit" class="btn btn-primary">Save changes</button>
-      </form>
+      <TheUserForm/>
 
       <h2>Password</h2>
       <p><router-link :to="{name: 'password_change'}" class="btn btn-primary">Change my password</router-link></p>
@@ -29,10 +27,12 @@ import { createNamespacedHelpers } from 'vuex'
 const { mapState, mapActions } = createNamespacedHelpers('user')
 
 import TheNavbar from './TheNavbar.vue'
+import TheUserForm from './TheUserForm.vue'
 
 export default {
   components: {
-    TheNavbar
+    TheNavbar,
+    TheUserForm
   },
   computed: mapState([
     'auth_token'
---
+++ web/frontend/src/components/TheUserForm.vue
@@ -0,0 +1,36 @@
+<template>
+<form>
+  <div class="mb-3">
+    <label for="first_name" class="form-label">First name</label>
+    <input v-model="first_name" id="first_name" type="text" class="form-control">
+  </div>
+  <div class="mb-3">
+    <label for="last_name" class="form-label">Last name</label>
+    <input v-model="last_name" id="first_name" type="text" class="form-control">
+  </div>
+  <div class="mb-3">
+    <label for="email" class="form-label">Email</label>
+    <input v-model="email" id="first_name" type="text" class="form-control">
+  </div>
+  <button type="submit" class="btn btn-primary">Save changes</button>
+</form>
+</template>
+
+<script lang="ts">
+import { createNamespacedHelpers } from 'vuex'
+const { mapState, mapActions } = createNamespacedHelpers('user')
+
+export default {
+  computed: mapState([
+    'first_name',
+    'last_name',
+    'email'
+  ]),
+  methods: {
+    ...mapActions(['resetToken'])
+  }
+}
+</script>
+
+<style>
+</style>
