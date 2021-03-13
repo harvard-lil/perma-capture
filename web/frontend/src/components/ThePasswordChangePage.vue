91b67d4a4a2110a60d10e7bb99c13155bb88f733
Greg Leppert <greg@leppert.me>
2021-03-13 16:01:48 -0500

    Rough in password change form

--- web/frontend/src/components/ThePasswordChangePage.vue
+++ web/frontend/src/components/ThePasswordChangePage.vue
@@ -1,6 +1,21 @@
 <template>
 <TheMainHeader
   head="Change password"/>
+<form>
+  <div class="mb-3">
+    <label for="old_password" class="form-label">Old password</label>
+    <input v-model="old_password" id="old_password" type="text" class="form-control">
+  </div>
+  <div class="mb-3">
+    <label for="new_password1" class="form-label">New password</label>
+    <input v-model="new_password1" id="new_password1" type="text" class="form-control">
+  </div>
+  <div class="mb-3">
+    <label for="new_password2" class="form-label">New password confirmation</label>
+    <input v-model="new_password2" id="new_password2" type="text" class="form-control">
+  </div>
+  <button type="submit" class="btn btn-primary">Change my password</button>
+</form>
 </template>
 
 <script lang="ts">
@@ -11,7 +26,7 @@ export default {
     TheMainHeader
   }
 }
-</script>c
+</script>
 
 <style>
 </style>
