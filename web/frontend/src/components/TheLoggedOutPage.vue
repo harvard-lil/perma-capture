7e12ce67966f36cc7cd68e3fc32bf6bcf91cc111
Greg Leppert <greg@leppert.me>
2021-03-15 10:11:38 -0400

    Add a logged out vue page

---
+++ web/frontend/src/components/TheLoggedOutPage.vue
@@ -0,0 +1,19 @@
+<template>
+<TheMainHeader
+  head="Logged out"/>
+You have logged out.
+<router-link :to="{name: 'login'}">Log in again</router-link>
+</template>
+
+<script lang="ts">
+import TheMainHeader from './TheMainHeader.vue'
+
+export default {
+  components: {
+    TheMainHeader
+  }
+}
+</script>
+
+<style>
+</style>
--- web/frontend/src/router.ts
+++ web/frontend/src/router.ts
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
 import TheRoot from './components/TheRoot.vue'
 import TheDocsPage from './components/TheDocsPage.vue'
 import TheSignUpPage from './components/TheSignUpPage.vue'
 import TheLoginPage from './components/TheLoginPage.vue'
+import TheLoggedOutPage from './components/TheLoggedOutPage.vue'
 import TheAccountSettingsPage from './components/TheAccountSettingsPage.vue'
 import ThePasswordChangePage from './components/ThePasswordChangePage.vue'
 import ThePasswordResetPage from './components/ThePasswordResetPage.vue'
@@ -12,6 +14,7 @@ const routes = [
   { path: '/docs/', name: 'docs', component: TheDocsPage },
   { path: '/sign-up/', name: 'sign_up', component: TheSignUpPage },
   { path: '/user/login/', name: 'login', component: TheLoginPage },
+  { path: '/user/logout/', name: 'logout', component: TheLoggedOutPage },
   { path: '/user/account/', name: 'account', component: TheAccountSettingsPage },
   { path: '/user/password_change/', name: 'password_change', component: ThePasswordChangePage },
   { path: '/user/password_reset/', name: 'password_reset', component: ThePasswordResetPage }
