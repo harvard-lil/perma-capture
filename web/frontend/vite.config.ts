880a7fc664bb6dc21f56e740a68fae3f0ac737ed
Greg Leppert <greg@leppert.me>
2021-04-13 09:48:27 -0400

    Specify vite base in config rather than flag

--- web/frontend/vite.config.ts
+++ web/frontend/vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   return {
-    base: mode == 'development' ? '/vite/' : null,
+    base: mode == 'development' ? '/vite/' : '/static/',
     plugins: [vue({
       template: {
         compilerOptions: {
