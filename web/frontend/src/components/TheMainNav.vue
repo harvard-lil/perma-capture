4425bd0fe6b7b3428bff35b303f10d3518e34995
anastasia <anastasia.aizman@gmail.com>
2021-07-26 13:56:56 -0400

    bump - hiding celery link

--- web/frontend/src/components/TheMainNav.vue
+++ web/frontend/src/components/TheMainNav.vue
@@ -1,33 +1,49 @@
 <template>
-<div v-if="is_authenticated" class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-    {{ first_name }}
-  </button>
-  <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton1">
-    <li><router-link :to="{name: 'root'}" class="dropdown-item">Dashboard</router-link></li>
-    <li><router-link :to="{name: 'account'}" class="dropdown-item" href="/user/account/">My Account</router-link></li>
-    <li><router-link :to="{name: 'docs'}" class="dropdown-item">User Guide</router-link></li>
-    <li><a class="dropdown-item" href="/user/logout/">Log Out</a></li>
-    
-    <template v-if="is_staff">
-      <li><hr class="dropdown-divider"></li>
-      <li><h6 class="dropdown-header">Admin Only</h6></li>
-      <li><a class="dropdown-item" href="/admin/">Django Admin</a></li>
-      <li><a class="dropdown-item" href="/manage/celery/">Celery Queue Status</a></li>
-    </template>
-  </ul>
-</div>
+  <div v-if="is_authenticated" class="dropdown">
+    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown"
+            aria-expanded="false">
+      {{ first_name }}
+    </button>
+    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton1">
+      <li>
+        <router-link :to="{name: 'root'}" class="dropdown-item">Dashboard</router-link>
+      </li>
+      <li>
+        <router-link :to="{name: 'account'}" class="dropdown-item" href="/user/account/">My Account</router-link>
+      </li>
+      <li>
+        <router-link :to="{name: 'docs'}" class="dropdown-item">User Guide</router-link>
+      </li>
+      <li><a class="dropdown-item" href="/user/logout/">Log Out</a></li>
 
-<ul v-else class="navbar-nav">
-  <li class="nav-item"><router-link :to="{name: 'sign_up'}" class="nav-link" href="/sign-up/">Sign up</router-link></li>
-  <li class="nav-item"><router-link :to="{name: 'docs'}" class="nav-link">User Guide</router-link></li>
-  <li class="nav-item"><router-link :to="{name: 'login'}" class="nav-link" href="/user/login">Log in</router-link></li>
-</ul>
+      <template v-if="is_staff">
+        <li>
+          <hr class="dropdown-divider">
+        </li>
+        <li><h6 class="dropdown-header">Admin Only</h6></li>
+        <li><a class="dropdown-item" href="/admin/">Django Admin</a></li>
+        <!-- <li><a class="dropdown-item" href="/manage/celery/">Celery Queue Status</a></li>-->
+      </template>
+    </ul>
+  </div>
+
+  <ul v-else class="navbar-nav">
+    <li class="nav-item">
+      <router-link :to="{name: 'sign_up'}" class="nav-link" href="/sign-up/">Sign up</router-link>
+    </li>
+    <li class="nav-item">
+      <router-link :to="{name: 'docs'}" class="nav-link">User Guide</router-link>
+    </li>
+    <li class="nav-item">
+      <router-link :to="{name: 'login'}" class="nav-link" href="/user/login">Log in</router-link>
+    </li>
+  </ul>
 </template>
 
 <script lang="ts">
-import { createNamespacedHelpers } from 'vuex'
-const { mapState } = createNamespacedHelpers('user')
+import {createNamespacedHelpers} from 'vuex'
+
+const {mapState} = createNamespacedHelpers('user')
 
 export default {
   computed: mapState([
@@ -39,10 +55,10 @@ export default {
 </script>
 
 <style scoped>
-  .dropdown-toggle {
-    background-color: white;
-    color: black;
-    border-color: var(--color-background);
-    padding: 1em;
-  }
+.dropdown-toggle {
+  background-color: white;
+  color: black;
+  border-color: var(--color-background);
+  padding: 1em;
+}
 </style>
